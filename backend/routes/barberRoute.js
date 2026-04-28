5e9b7c247c5e7911fd4bf5972470ce1fa8de6438
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-04-28 14:16:40 +0530

    multer middleware for image upload

--- backend/routes/barberRoute.js
+++ backend/routes/barberRoute.js
@@ -1,11 +1,16 @@
 import express from "express";
-import { addBarber, listBarbers, removeBarber } from "../controller/barberController.js";
+import {
+  addBarber,
+  listBarbers,
+  removeBarber,
+} from "../controller/barberController.js";
 import adminAuth from "../middleware/authAdmin.js";
+import upload from "../middleware/multer.js";
 
 const barberRouter = express.Router();
 
-barberRouter.post("/add",adminAuth, addBarber);
+barberRouter.post("/add", adminAuth, upload.single("image"), addBarber);
 barberRouter.get("/list", listBarbers);
-barberRouter.post("/remove",adminAuth,removeBarber)
+barberRouter.post("/remove", adminAuth, removeBarber);
 
 export default barberRouter;
