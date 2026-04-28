56b04bc0b21ba7a88d44114065dcd0cab4d67176
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-04-28 20:19:20 +0530

    created bookAppointment controller function

---
+++ backend/controller/appointmentController.js
@@ -0,0 +1,38 @@
+import AppointmentModel from "../models/appointmentModel.js";
+import BarberModel from "../models/barberModel.js";
+import UserModel from "../models/userModel.js";
+
+const bookAppointment = async (req, res) => {
+  try {
+    const { userId } = req.headers;
+    const { barberId, date, time } = req.body;
+    // get barber data
+    const barber = await BarberModel.findById(barberId);
+
+    if (!barber.availability) {
+      return res.json({ success: false, message: "Barber not available" });
+    }
+
+    // get user data
+    const user = await UserModel.findById(userId).select("-password");
+
+    const appointement = new AppointmentModel({
+      userId,
+      barberId,
+      userData: user,
+      barberData: barber,
+      date,
+      time,
+      fees: barber.fees, //take fees from barber not user input
+    });
+
+    await appointement.save();
+
+    res.json({ success: true, message: "Appointment booked successfully" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export { bookAppointment };
