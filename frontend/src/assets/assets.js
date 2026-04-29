684924e178cc1d48be3b401fea063ca305a245a1
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-04-29 20:20:21 +0530

    Added all images and import in a single file

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,35 @@
+import logo from "./logo.svg";
+import barber1 from "./barber1.jpg";
+import barber2 from "./barber2.jpg";
+import barber3 from "./barber3.jpg";
+import barber4 from "./barber4.jpg";
+import barber5 from "./barber5.jpg";
+import img1 from "./img1.jpg";
+import img2 from "./img2.jpg";
+import img3 from "./img3.jpg";
+import img4 from "./img4.jpg";
+import img5 from "./img5.jpg";
+import img6 from "./img6.jpg";
+import img7 from "./img7.jpg";
+import img8 from "./img8.jpg";
+import img9 from "./img9.jpg";
+import img10 from "./img10.jpg";
+
+export const assets = {
+  logo,
+  barber1,
+  barber2,
+  barber3,
+  barber4,
+  barber5,
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img7,
+  img8,
+  img9,
+  img10,
+};
