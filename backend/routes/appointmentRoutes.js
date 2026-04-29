70cc74a3e0e97c9c68a2924e9393d616597ac746
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-04-29 09:18:54 +0530

    Completed all the backend logic

--- backend/routes/appointmentRoutes.js
+++ backend/routes/appointmentRoutes.js
@@ -2,14 +2,19 @@ import express from "express";
 import {
   bookAppointment,
   cancelAppointment,
+  listAllAppointment,
   listUserAppointment,
+  updateAppointmentStatus,
 } from "../controller/appointmentController.js";
 import userAuth from "../middleware/authUser.js";
+import adminAuth from "../middleware/authAdmin.js";
 
 const appointmentRouter = express.Router();
 
 appointmentRouter.post("/book", userAuth, bookAppointment);
 appointmentRouter.get("/list", userAuth, listUserAppointment);
 appointmentRouter.post("/cancel", userAuth, cancelAppointment);
+appointmentRouter.get("/list-all", adminAuth, listAllAppointment);
+appointmentRouter.post("/status", adminAuth, updateAppointmentStatus);
 
 export default appointmentRouter;
