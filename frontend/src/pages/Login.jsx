e2438c50b1662baea8bcf1b767a057487b7516e1
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-04-30 19:50:54 +0530

    Implement user login form with error handling and dynamic state

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,22 @@
+import { useContext, useState } from "react";
+import { AppContext } from "../context/AppContext";
+import { toast } from "react-toastify";
+
 const Login = () => {
+  const { setToken, navigate, backendUrl } = useContext(AppContext);
+  const [currentState, setCurrentState] = useState("login");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmitHandler = async (e) => {
+    try {
+      e.preventDefault();
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
   return (
     <>
       <section className="bg-gray-50 dark:bg-gray-900">
@@ -16,10 +34,34 @@ const Login = () => {
           </a>
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                Create an account
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white text-center">
+                {currentState == "login" ? "User Login" : "Create an account"}
               </h1>
-              <form className="space-y-4 md:space-y-6" action="#">
+              <form
+                onSubmit={onSubmitHandler}
+                className="space-y-4 md:space-y-6"
+                action="#"
+              >
+                {currentState != "login" && (
+                  <div>
+                    <label
+                      htmlFor="name"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Your name
+                    </label>
+                    <input
+                      onChange={(e) => setName(e.target.value)}
+                      value={name}
+                      type="text"
+                      name="name"
+                      id="name"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#865926] focus:border-[#865926] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      placeholder="Your name"
+                      required=""
+                    />
+                  </div>
+                )}
                 <div>
                   <label
                     htmlFor="email"
@@ -28,11 +70,13 @@ const Login = () => {
                     Your email
                   </label>
                   <input
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
                     type="email"
                     name="email"
                     id="email"
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#865926] focus:border-[#865926] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="name@company.com"
+                    placeholder="Your email"
                     required=""
                   />
                 </div>
