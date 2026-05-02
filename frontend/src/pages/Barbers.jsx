0c582b2ee4e837152b193309873dbb5c611c0105
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-05-02 14:13:32 +0530

    Completed barbers page

--- frontend/src/pages/Barbers.jsx
+++ frontend/src/pages/Barbers.jsx
@@ -1,8 +1,19 @@
+import { useContext } from "react";
+import { AppContext } from "../context/AppContext";
+import BarberCart from "../components/BarberCart";
 
 const Barbers = () => {
+  const { barbers } = useContext(AppContext);
   return (
-    <div>Barbers</div>
-  )
-}
+    <div className="p-10">
+      <h1 className="text-2xl font-bold mb-6">Our Barbers</h1>
+      <div className="grid grid-cols-5 gap-4">
+        {barbers.map((barber) => (
+          <BarberCart key={barber._id} barber={barber} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Barbers
+export default Barbers;
