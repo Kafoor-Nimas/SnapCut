050b8211dfd30ae10c0bbfaf45b7c2613f9b708b
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-05-02 18:06:31 +0530

    Implement routing and authentication in App component; add Login component and environment configuration

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,37 @@
+import { useState } from "react";
 import Navbar from "./components/Navbar";
+import { ToastContainer } from "react-toastify";
+import Login from "./components/Login";
+import Sidebar from "./components/Sidebar";
+import { Routes, Route } from "react-router-dom";
+import AddBarber from "./pages/AddBarber";
+import BarbersList from "./pages/BarbersList";
+import Dashboard from "./pages/BarbersList";
+import Appointments from "./pages/Appointments";
 
 const App = () => {
+  const [token, setToken] = useState(localStorage.getItem("token") || "");
   return (
     <div>
-      <Navbar />
-      
+      <ToastContainer />
+      {token === "" ? (
+        <Login setToken={setToken} />
+      ) : (
+        <>
+          <Navbar setToken={setToken} />
+          <div className="flex">
+            <Sidebar />
+            <div className="flex-1 p-8">
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/add-barber" element={<AddBarber />} />
+                <Route path="/barbers" element={<BarbersList />} />
+                <Route path="/appointments" element={<Appointments />} />
+              </Routes>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
