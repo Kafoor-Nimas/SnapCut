ec6e9803bb9116606c36c72324809b1fe0e31783
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-05-05 11:38:51 +0530

    fetch all appointments

--- admin/src/pages/Appointments.jsx
+++ admin/src/pages/Appointments.jsx
@@ -1,8 +1,31 @@
+import axios from "axios";
+import { useState } from "react";
+import { backendUrl } from "../App";
+import { toast } from "react-toastify";
 
-const Appointments = () => {
-  return (
-    <div>Appointments</div>
-  )
-}
+const Appointments = ({ token }) => {
+  const [appointments, setAppointments] = useState([]);
 
-export default Appointments
+  const getAppointments = async () => {
+    try {
+      const response = await axios.get(
+        backendUrl + "/api/appointment/list-all",
+        { headers: token },
+      );
+
+      if (response.data.success) {
+        setAppointments(response.data.appointments);
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
+  
+  return <div>Appointments</div>;
+};
+
+export default Appointments;
