09197f7bef13fa381d414cef5be326daec3bb7e9
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-05-06 10:04:12 +0530

    Make the barber page mobile responsive

--- frontend/src/pages/Barbers.jsx
+++ frontend/src/pages/Barbers.jsx
@@ -7,7 +7,7 @@ const Barbers = () => {
   return (
     <div className="p-10">
       <h1 className="text-2xl font-bold mb-6">Our Barbers</h1>
-      <div className="grid grid-cols-5 gap-4">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 items-start">
         {barbers.map((barber) => (
           <BarberCart key={barber._id} barber={barber} />
         ))}
