05bbccb82fe7ba35f570087b46ce93fc16e84056
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-05-06 09:51:46 +0530

    Completed meet our barber section responsive

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,11 +8,11 @@ const Home = () => {
   return (
     <div>
       {/* Hero Section */}
-      <div className="bg-[#171008] flex items-center justify-between px-16 py-20">
+      <div className="bg-[#171008] flex flex-col md:flex-row items-center justify-between px-8 md:px-16 py-12 md:py-20 gap-8">
         {/* Left Side Text */}
-        <div className="max-w-lg">
+        <div className="max-w-lg text-center md:text-left">
           {/* Heading */}
-          <h1 className="text-5xl font-bold text-white leading-tight">
+          <h1 className="text-3xl md:text-5xl font-bold text-white leading-tight">
             Skilled Craftsmen <br />
             <span className="text-[#c8a063]">Barbershop</span>
           </h1>
@@ -31,21 +31,15 @@ const Home = () => {
         {/* Right side -image */}
         <div className="flex gap-4">
           <img
-            // src={assets.img8}
             src={assets.banner2}
             alt=""
             className="h-80 rounded-lg object-cover"
           />
-          {/* <img
-            src={assets.img9}
-            alt=""
-            className="h-64 rounded-lg object-cover mt-16"
-          /> */}
         </div>
       </div>
 
       {/* Meet Our Barbers */}
-      <div className="flex flex-col gap-6 my-10 p-10">
+      <div className="flex flex-col gap-6 my-6 px-4 md:px-10">
         <div className="flex justify-between">
           <h1 className="text-3xl font-bold text-[#c8a063]">
             Meet Our <span className="">Barbers</span>
@@ -58,7 +52,7 @@ const Home = () => {
           </button>
         </div>
 
-        <div className="grid grid-cols-4 lg:grid-cols-5 gap-4 ">
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
           {barbers.slice(0, 5).map((barber) => (
             <BarberCart key={barber._id} barber={barber} />
           ))}
